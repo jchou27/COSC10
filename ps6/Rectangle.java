bba66ee9f81333346c5391ac363953abdecb5c4e
jchou27 <jack.h.chou.27@dartmouth.edu>
2024-02-29 16:58:28 -0500

    PS6 Commit

---
+++ ps6/Rectangle.java
@@ -0,0 +1,37 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
+/**
+ * A rectangle-shaped Shape
+ * Defined by an upper-left corner (x1,y1) and a lower-right corner (x2,y2)
+ * with x1<=x2 and y1<=y2
+ * 
+ * @author Chris Bailey-Kellogg, Dartmouth CS 10, Fall 2012
+ * @author CBK, updated Fall 2016
+ * @author Tim Pierson Dartmouth CS 10, provided for Winter 2024
+ */
+public class Rectangle implements Shape {
+	// TODO: YOUR CODE HERE
+	@Override
+	public void moveBy(int dx, int dy) {
+	}
+
+	@Override
+	public Color getColor() {
+	}
+
+	@Override
+	public void setColor(Color color) {
+	}
+		
+	@Override
+	public boolean contains(int x, int y) {
+	}
+
+	@Override
+	public void draw(Graphics g) {
+	}
+
+	public String toString() {
+	}
+}
